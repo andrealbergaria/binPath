3ac4d3d89798c7b1d758f09cd56a0aecaa934ca9
andre albergaria coelho <andrealbergaria@gmail.com>
2020-10-08 21:20:58 +0100

    testUpload2

--- upload.sh
+++ upload.sh
@@ -10,5 +10,5 @@ git add .
 git commit -m $1
 
 USER="andrealbergaria";
-git push https://github.com/andrealbergaria/binPath < andrealbergaria
+git push https://github.com/andrealbergaria/binPath < $USER
 
