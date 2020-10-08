664a3c958b8ac26a537b98f15f8d04d0ca92a0a5
andre albergaria coelho <andrealbergaria@gmail.com>
2020-10-08 21:43:33 +0100

    Testing xor with seq

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [[ -z $1 ]];
 then
     echo -e "Must set commit msg on arg\n";
@@ -7,8 +8,7 @@ fi
 
 git add .
 
-git commit -m $1
-
-
-echo "andrealbergaria" | git push https://github.com/andrealbergaria/binPath
+git commit -m "$1"
+echo -e "\nCommited git repo with msg "$1
+git push https://github.com/andrealbergaria/binPath
 
