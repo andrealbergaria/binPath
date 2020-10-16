bb7eaa8f2064bde4a765502a8176cd95b0159394
andre albergaria coelho <andrealbergaria@gmail.com>
2020-10-16 23:54:06 +0100

    can concat bytes

--- binPath.c
+++ binPath.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
 
 int main() {
+            /* algorithm       algorithm
+      a)  [0] Bit bit Bit [0] bit bit bits (64 2^6); (2nd igual ao 1)
+             
+      b)  [0] bit bit bit [1] bit bit bits
+             
+      c)  [1] bit bit bit [0] bit bit bit
+             
+      d)  [1] bit bit bit [1] bit bit bit (2nd igual ao primeiro)
+      algorithm = start wth 000, and change one bit, until can't change nomre...then use 111 and change one bit of it
+      
+      a) [0] bit bit bit [0] bit bit bit [0] bit bit bit [0] bit bit bit algortuihm funciona ..so preciso concatenar
+           
+            
+            
+      Para 32 bytes = 2^
+      1 byte -> 64
+      4bytes -> x
+      x= 256
+      
+ 1 byte -> 4 combos
+ 2 bytes -> X
+ x=8
+ 
+    two bytes
     
+    e) a) a)
+    f) b) b)
+    g) c) c)
+    h) d) d)
+ 
+      16 bytes
+      
+ 
+    char *ptr_1_ = 
+     int alg[] = {0,1,2,3,4,5,6,7};
+     int *alg2 = alg;
+     int *ptr = alg;
+     char prefix =0 ;
+     printf("\n");
+            for (int idx=0 ; idx < 4 ; idx++) {
+                printf("%i ",prefix);
+                
+            }
+            
+      for (int idx=0 ; idx < 4 ; idx++) {
+          three_bits[0]
+      }
+       
 }
+
