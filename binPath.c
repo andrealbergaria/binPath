b500e776a191315fa22ca7ce3ec726036005b292
andre albergaria coelho <andrealbergaria@gmail.com>
2020-10-23 02:39:49 +0100

    trying to scan without scanning base

--- binPath.c
+++ binPath.c
@@ -2,6 +2,128 @@
 #include "util.h"
 #include <string.h>
 #include <stdlib.h>
+
+
+void printFullArray() {
+    
+    char base[] = {0b000,0b001,0b010,0b100,0b110,0b111,0b101,0b001};
+    char temp[8]= {0b000,0b001,0b010,0b100,0b110,0b111,0b101,0b001};
+    unsigned int v[8][1];
+    char *tempVar = (char *) malloc(500);
+    tempVar = base;
+    
+    
+    
+    
+    
+   /* v[0][0] = base[0]; // 0
+    v[0][1] = base[1]; // 2    
+    v[0][2] = base[2]; // 4
+    v[0][3] = base[3]  // 8
+    
+ 
+    var = [elems] | base[]
+    
+    var = ELeems | 000
+    var = ELeems | 001
+    var = ELeems | 002
+    var = ELeems | 003
+    
+    var = 0 | 000 
+    var = 0  | 001 
+    var = 0 | 010
+       v[it] = 0,1
+    var = 1 | 00  // so precisa mudar 1 elemento da array
+    */
+   v[0][0] =  
+    for (int it=0; it < 8 ; it++) {
+        //temp[it] = temp[] base[] 
+        
+        v[it]++;
+        v[it][it]   &base;
+        /*temp[it][it]++;
+        temp[it][it] = *tempVar;
+        *tempVar++;*/
+        }
+    }
+}
+
+void print_6_bits() {
+           
+     char b[8][8];
+     char c[8][8];
+     char a[8][8];
+     
+     for (int set=0 ; set < 8 ; set++) {
+        a[set][0] = set;
+        b[set][0] = set;    // fill the last element array with the algorithm
+        c[set][0] = set;
+    //    printf("a[%i] -> ",set);
+        
+     //   printBits(a[set][0]);
+     //  printf("\n");
+     //   printf("b[%i] -> ",set);
+        
+      //  printBits(b[set][0]);
+      //  printf("\n");
+      //  printf("c[%i] -> ",set);
+        
+      //  printBits(c[set][0]);
+       // printf("\n");
+     }
+     //printf("\nFilled array ...\n");
+     
+     printf("\n\nEntering cycles\n");
+     
+     for (int s= 0 ; s < 8; s++) {
+            b[s][s]++;
+            c[s][s] = b[s][s]++;
+            
+            printf("a[%i][0] -> ",s);
+            printBits(a[s][0]);
+            printf("\n");
+            printf("b[%i][0] -> ",s);
+            printBits(b[s][0]);
+            printf("\n");
+            printf("c[%i][0] -> ",s);
+            printBits(c[s][0]);
+            printf("\n");
+            a[s][0]++;
+            
+            
+            
+     }
+     
+     
+}
+
+
+void printStrings() {
+     
+                            char src[] = "[0] 0 1 2 3 4 5 6 7";
+                            char *dest = (char* ) malloc(10000);
+                            
+                            // prottpye 
+                            // prtotype (char *dest, const char *src);
+                            // char *strncat(char *dest, const char *src, size_t n);
+
+                            dest[10000] = '\0';
+                            
+                            strncat(dest,src,16);
+                            
+                            //The strcat function returns a pointer to s1 (dest) (where the resulting concatenated string resides).
+                           // 24 bits / 3 bits = 8 cycles
+                            
+                            for (int idx= 0; idx < 8; idx++) {
+                                strncat(dest,src,21);
+                                printf(" %s ",dest);
+                            }
+                        
+                        
+                            printf("\n\n\n");
+                        
+}
+
 int main() {
     //algorithm = start wth 000, and change one bit, until can't change nomre...then use 111 and change one bit of it
             /* algorithm       
@@ -19,59 +141,36 @@ int main() {
         char *ptr = &a[0];
         
         
-  a[0][0] 000 000           a[0][0] = 000 
-  a[1][0] 000 001
-  a[2][0] 000 010         
-  a[3][0] 000 100         
-  a[4][0] 000 111
-  a[5][0] 000 110
-  a[6][0] 000 101
-  a[7][0] 000 011
-     
-  b[0][0] 000 000
-  b[1][0] 001 000  b[0][0]  = a[0][0];
-  b[2][0] 001 001
-  b[3][0] 001 010          b[2][0] = a[2][0] = 010,010,010
-  b[4][0] 001 100          b[3][0] = a[3][0] = 100,100,100
-  b[5][0] 001 111
-  b[6][0] 001 110
-  b[7][0] 001 101
-  b[8][0] 001 011
+  a[0][0] 000 000         
+  a[1][1] 000 001            
+  a[2][2] 000 010         
+  a[3][3] 000 100      X ->  last array doesnt change
+                        it = 0 
+  a[4][4] 000 111       a[1][1] = b[1][1] = c[1][1]
+                        a[3][3] = b[4][3] = c[3][3] 
+                        000 100   
+  a[5][5] 000 110       a[5][5] =  b [5] [B]  = c[5][c5] 
+  a[6][6] 000 101       000 110   001 110           010 110
+  a[7][7] 000 011
+                        a[5][5] = 000 110
+  b[0][0] 001 000       b[5][5] = 001 110
+  b[1][1] 001 001       //using set
+  b[2][2] 001 010
+  b[3][3] 001 100       b[5][5] = 
+  b[4][4] 001 111
+  b[5][5] 001 110
+  b[6][6] 001 101
+  b[7][7] 001 011
     
-  c[0] 010 000  
-  c[1] 010 001
-  c[2] 010 010  
-  c[3] 010 100
-  c[4] 010 111
-  c[5] 010 110
-  c[6] 010 010
-  c[7] 010 100
+  c[0][0] 010 000  
+  c[1][1] 010 001
+  c[2][2] 010 010  
+  c[3][3] 010 100
+  c[4][4] 010 111
+  c[5][5] 010 110
+  c[6][6] 010 010
+  c[7][7] 010 100
   
-  a[0][0] = b[0][0] = c[0][0]
-  a[2][0] = b[2][0] = c[2][0];
-  
-  sencond emelemtn always 000,001,010,100,111,110,101,011
-  
-  a[1][0] = 000
-  b[1][0] = 
-  
-     char a[8][8];
-     char b[8][8];
-     char c[8][8];
-     a[0][0] = 0;
-     
-     for (int set=0 ; set < 8 ; set++) {
-        a[set][0] = set;
-        b[set][0] = set;    // fill the last element array with the algorithm
-        c[set][0] = set;
-     }
-     for (int s= 0 ; s < 8; s++) {
-            b[s][0] = a[s][0];
-            c[s][0] = b[s][0];
-            a[s][0]++;
-     }
-     
- 
      
       a) [0] bit bit bit [0] bit bit bit [0] bit bit bit [0] bit bit bit
            
@@ -88,69 +187,7 @@ int main() {
            X       11bits
            11 / 8 = 
            
-                            A    B   C   D
-                            000 000 000 000
-                            001 001 001 001
-                            010 010 010 010
-                            100 100 100 100
-                            111 111 111 111
-                            110 110 110 110
-                            101 101 101 101
-                            011 011 011 011
-                          
-                            trying algorithm with 3 letters
-                            
-                               000 000 000 000  => 000
-                               000 000 000 001  => 001
-                               000 000 000 010  => 010
-                               000 000 000 100  => 100
-                                                   111
-                                                   110
-                                                   101
-                                                   011
-                                                   
-                            
-                                                   
-                                    
-                            
-                            
-                            A=B
-                            
-                            Para 3 bits
-                            A
-                            A+1
-                            A+2
-                            A+3
-                            A+4
-                            A+5
-                            A+6
-                            A+7
-                            
-                            Para 6 bits
-                            A   "A A+1 A+2 A+3 A+4 A+5 A+6 A+7"
-                            A+1 "A A+1 A+2 A+3 A+4 A+5 A+6 A+7"
-                            A+2 "A A+1 A+2 A+3 A+4 A+5 A+6 A+7"
-                            A+3 "A A+1...."
-                            A+4 "A A+1 ..."
-                            A+5 " A A+1..."
-                            A+6 "A A+1;
-                            
-                            ou para mais coerencie supondo que A e B represtam 3bits
-                            A "B B+1 B+2 B+3 B+4 B+5 B+6 B+7"
-                            A= B entao basta subtituir
-                            
-                            8^3 => 512 (A*A*A) (being A=3bits)
-                            9bits=> 512 ones and zeros
-                            
-                            Para 9 bits
-                            A   "A A+1 A+2 A+3 A+4 A+5 A+6 A+7" "A A+1 A+2 A+3 A+4 A+5 A+6 A+7"
-                            A+1 "A A+1 A+2 A+3 A+4 A+5 A+6 A+7"
-                            A+2 "A A+1 A+2 A+3 A+4 A+5 A+6 A+7"
-                            A+3 "A A+1...."
-                            A+4 "A A+1 ..."
-                            A+5 " A A+1..."
-                            A+6 "A A+1;
-                            
+                           
                             char str[] = {0b000,0b001,0b010,0b100,0b101,0b110,0b011,0b111,'\0'};
                             char *src = str;
                             char *dest = (char* ) malloc(10000);
@@ -178,29 +215,10 @@ int main() {
        32 bits => 2^4 = 16
        256 bits =  128 comb of 0 and 1 (prefixes)
        256 / 8 bits = 32bytes
-       32bytes -> 1byte
-       1 -> x */
        
-            
-       char src[] = "[0] 0 1 2 3 4 5 6 7";
-                            char *dest = (char* ) malloc(10000);
-                            
-                            // prottpye 
-                            // prtotype (char *dest, const char *src);
-                            // char *strncat(char *dest, const char *src, size_t n);
+       */                   
 
-                            dest[10000] = '\0';
-                            
-                            strncat(dest,src,16);
-                            
-                            //The strcat function returns a pointer to s1 (dest) (where the resulting concatenated string resides).
-                           // 24 bits / 3 bits = 8 cycles
-                            
-                            for (int idx= 0; idx < 8; idx++) {
-                                strncat(dest,src,21);
-                                printf(" %s ",dest);
-                            }
-                 
+     print_6_bits();
         
 }
 
--- util.h
+++ util.h
@@ -2,4 +2,6 @@
 void convertByteToAscii(unsigned char byte,char mask);
 void printBits(unsigned char number);
 void printShortBits(unsigned short number); 
+void print_6_bits();
+void printStrings();
     
