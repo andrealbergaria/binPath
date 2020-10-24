3de3ec5c0da7fd931e4d3f3c6e3a1655c3c561c9
andre albergaria coelho <andrealbergaria@gmail.com>
2020-10-24 03:49:01 +0100

    tweaked pritnAllCombs

--- printAllCombs.c
+++ printAllCombs.c
@@ -2,21 +2,32 @@
 
 int main(int argc,char *argv[]) {
     
-    if (argc != 3) {
-        printf(" ARG[1] -> number_of_bits_to_print ");
-        printf(" ARG[2] -> start_byte ");
+    if (argc != 4) {
+        printf(" ARG[1] -> number_of_bits_to_print\n");
+        printf(" ARG[2] -> start_byte\n");
+        printf(" ARG[3] -> wheter print bytes in new line or not (new | listed)\n");
         return -1;
     }
     int startByte = atoi(argv[2]);
     int numberOfBits = atoi(argv[1]);
+    
+    
     printf("\nUsing %i bits\n",numberOfBits);
     printf("\nStarted printing at %i",startByte);
+    
     int s = pow(2,numberOfBits);
+    
     printf("\nNumber of elems printed %i",s);
-    printf("\n{ ");
+    
+    if (strcmp(argv[3],"listed")==0)
+        printf("\n{ ");
+    
     for (int i = 0; i< s ; i++) {
         printBits(startByte);
-        printf(" , ");
+        if (strcmp(argv[3],"listed")==0)
+            printf(" , ");
+        else
+            printf("\n");
         startByte++;
     }
     printf(" } \n");
