f56694c647374ba71bb06dabc70987435f6b6edf
andre albergaria coelho <andrealbergaria@gmail.com>
2020-10-26 22:52:01 +0000

    Tweking check32bytes...and also tweaked table.bin

---
+++ errors/testLineConitnuation.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+        unsigned int a = 0b000 \
+                         000 ;
+}
--- util.c
+++ util.c
@@ -58,119 +58,65 @@ char check32Bytes() {
  //  {8, 9, 10, 11}   /*  initializers for row indexed by 2 */
 // };
 
-//64 bits * X  = 256 .. X = 4
-   
-     
-     /*
-     * 64 bits
-     * bytes  = 64 / 8 = 8 bytes
-     * 
-     * Positions          1             2               3           4           5       6           7           8
-     * Values             0             1               2           3           4       5           6           7
-       bitStrings 1)   00000000      00000001       00000010    000000100    00000101 00000110  00000111     00001000 (64bits)
+//64 bits * 4  = 256 ..
        
-       2) Values         8+1           8+2             8+3          8+4         8+5     8+6    8+7         8+8
-     *                 00001001      00001010       00001011   00001100     00001101 00001101  0000111      00010000
-
-       3) Values        8+8+1          8+8+2       8+8+3       8+8+4          8+8+5    8+8+6   8+8+7       8+8+8
-                       000100001    000010010       00010011   00010100     00010101  00010110 00010111     00011000
+    // Function for getting chars
+    // f(pos) = 8k+pos
      
-       9) Values        8*8+1
-                        
-     f(pos) = 8k+pos
-     
-     how many to add ? 64
-                        probably +1 beacuse of positions
-                        and the other?? 8 ?
-                        
-                        
-                        adicionar 8*8 a tabela anterior
-                        
-                        65 = 8*8+1 (pos 1)
-                        
-                        66 = 8*8+2 (pos 2)
-                        
-                        67 = 8*8+3 (pos 3)
-                        
-                        68 = 8*8 +4 (pos 4)
-                        
-                        69 = 8*8 +5 (pos 5)
-                        
-                        70 = 8*8 +6 (pos 6)
-                        
-                        71 = 8*8 +7 (ops 7)
-                        
-                        72 = 8*8 +8 (pos 8)
-                        
-                        anterior mais 1
-                        
-                        73 = 8*8 + 9
-                        
-                        8+1         8+2
-                        00001001 00001010 
-                        
-                        8*8+8    8*8+9
-                        1001000 
-                        
-                { 1,8*8}  =
-     
-                2*2*2 *2*2*2 *2*2
-                  8     8     4
-     
-                      Falta o prefixo de 1          
-                        
-                       
                     
-                    
-                    
-                    
-     64*8 =>  512bits => 64 bytes 1000000
-                                      
-        11111111 - 1000000  = 191  10111111
-        
-            
-    
-    
-    
-     position(1,2,3,4) = 
-     first_ int = 0000000 00000001 00000010 000000100
-     position(5,6,7,8) =
-     second_int = 000000101
-                  */  
-     //int one = {0,1,2,3}
-     //int two = {4,5,6,7}
-     //int three = {8+0,8+1,8+2,8+3}
-     
-     // FROM https://stackoverflow.com/questions/7787423/c-get-nth-byte-of-integer
-    //               int x = ((unsigned char *)(&number))[n];
+                        
      
 /*         
-  */   
-     
-     
-     //need 8 ints
-     
-    //     4 bytes   4 bytes  4bytes  4bytes 4bytes 4bytes 4bytes 4bytes       
-                
-   // {1,2,3,4}  {5,6,7,8} {9,10,11,12} {13,14,15,16} {17,18,19,20}   {21,22,23,24} {25,26,27,28} {29,30,3132}
-    //       1       2          3             4            5              6               7           8
     
-    
-       //all posistions = {numero of pos, 8+numero of possition,8+8+numerto
-          // position 1 => 1,8+1,8+8+1,8+8+8+1
-          //   position 2 => 2,2+8,2+8+8
+     
+     
+     //32 bytes need need 8 ints
+     
+          //all posistions = {numero of pos, 8+numero of possition,8+8+numerto
+          // FROM https://stackoverflow.com/questions/34357968/how-to-set-first-three-bytes-of-integer-in-c
+          /*int a = 4294967233;
+                char* p=&a;
+  
+                p[0] = whatever you wanted there
+                p[1] = whatever you wanted there
+          */
           
-   //      position is 1 for the moment
          printf("\n");
-         for (int pos = 1 ; pos < 9; pos++) {
-             printf("Pos %i\t",pos);
-         }
-         printf("\n");
-            for (int down=0; down < 32; down++) {
-                    int a  = pos+down*8;
+         // double res, %f 00000000      00000001       00000010    000000100
+         
+         
+         //                         0       1       2        3
+        unsigned int current = 0b00000000000000010000001000000011;
+         
+                                 
+                        
+         int mask = 0b1000000010000000100000001000;
+        
+    
+         int res1 =current | mask;
+            printf("\nMask : ");
+            printIntBits(mask); 
+            printf("\nMask Int : %i",mask);
+            printf("\n");
+            printIntBits(current);
+            printf("\nCurrent Int : %i",current);
+            printf("\n");
+            printIntBits(res1);
+            printf("\n");
+            printf("\nRES 1 int %i",res1);
+            // INT COMBS : 4294967296
+
+         int res2 = res1 | mask;
+            printf("\nres2\n2");
+            printIntBits(res1);
+         
+            for (int down=0; down < 4; down++) {
+                
+                
+                   // int a  = pos+down*8;
                    // printIntBits(a);
-                    printf("%i\t",a);
-                    
+                    if (down % 4 == 0)
+                        printf("\n");
             }
             
             
