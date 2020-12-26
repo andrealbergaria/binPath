ee65d96321340a0e59a616ce14e7350ad6f2a7fe
andre albergaria coelho <andrealbergaria@gmail.com>
2020-12-26 00:04:01 +0000

    refined partitions

---
+++ binPathJava/printPartitions.java
@@ -0,0 +1,17 @@
+package binPathJava;
+
+public class printPartitions {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void printPartitions(int numberOfPartitions) {
+		
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
